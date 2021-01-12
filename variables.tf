f4ddf42434d2e74cc33543be5ffe2adc32d3b06c
Kseniia Ryuma <kseniya0213@gmail.com>
2021-01-12 00:21:07 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -47,7 +47,7 @@ variable "width" {
 }
 
 variable "placeholder" {
-  default     = "https://loremflickr.com/g/320/240/coffeemug"
+  default     = "loremflickr.com"
   description = "Image-as-a-service URL; Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
 
