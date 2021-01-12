cf8d50c69ec5cd241e74745e68e992dab37af3b6
Kseniia Ryuma <kseniya0213@gmail.com>
2021-01-12 00:19:54 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@
 
 variable "prefix" {
   description = "This prefix will be included in the name of most resources."
-  default     = "hashi-mug"
+  default     = "coffeemug"
 }
 
 variable "region" {
