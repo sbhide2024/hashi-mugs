9b58b939b06ef914d17be0b5ed8d0b01a67a99f8
Kseniia Ryuma <kseniya0213@gmail.com>
2021-01-11 23:35:39 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -47,7 +47,7 @@ variable "width" {
 }
 
 variable "placeholder" {
-  default     = "https://res.cloudinary.com/digpynro3/image/upload/v1610423338/image_xoxnvp.jpg"
+  default     = "placekitten.com"
   description = "Image-as-a-service URL; Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
 
