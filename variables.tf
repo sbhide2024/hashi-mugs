3997c8f26330b52048c0203b56ff1c3a9fe89508
Kseniia Ryuma <kseniya0213@gmail.com>
2021-01-14 10:11:51 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@
 
 variable "prefix" {
   description = "This prefix will be included in the name of most resources."
-  default     = "hashimugs-app"
+  default     = "hashi-app"
 }
 
 variable "region" {
