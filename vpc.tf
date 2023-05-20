4de337e7adacda2501c50b315383c18029996f46
sbhide2024 <84491831+sbhide2024@users.noreply.github.com>
2023-05-20 11:26:05 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,10 @@
-module "vpc" {
-  source  = "app.terraform.io/sandtf/vpc/aws"
+# module "vpc" {
+ # source  = "app.terraform.io/sandtf/vpc/aws"
+  # version = "4.0.2"
+  module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
   version = "4.0.2"
-  
+}
   
   cidr = "${var.cidr}"
 }
